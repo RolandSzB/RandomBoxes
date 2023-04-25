9ccd7d5d8a8c4835e3d70fa0438eb2157561d03b
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-04-25 16:08:27 +0300

    random color made

--- scripts.js
+++ scripts.js
@@ -6,3 +6,10 @@ let boxes = [];
 function setup() {
   createCanvas(700, 700);
 }
+
+function randomColor() {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
